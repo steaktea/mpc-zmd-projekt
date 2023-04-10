b9a12a4ffb45663a899a27a74f27c9834a6c1b7a
Adam Ludes <211802@vut.cz>
2023-04-10 22:50:43 +0200

    improved layout

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from typing import List
 
 from PIL import Image
 from PIL.ImageQt import ImageQt
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import (
     QApplication,
@@ -13,10 +13,11 @@ from PyQt6.QtWidgets import (
     QFrame,
     QGridLayout,
     QLabel,
-    QMainWindow,
     QPushButton,
     QRadioButton,
+    QSizePolicy,
     QSlider,
+    QSpacerItem,
     QTabWidget,
     QVBoxLayout,
     QWidget,
@@ -25,41 +26,24 @@ from PyQt6.QtWidgets import (
 from image import ImageComponent, ImageData
 
 
-class ImageWindow(QMainWindow):
-    def __init__(self, image: Image, title):
-        super().__init__()
-        self.setWindowTitle(title)
-
-        self.image = image.convert("RGB")
-
-        widget = QWidget()
-        widget_layout = QVBoxLayout()
-        image_field = QLabel()
-        save_button = QPushButton("Save")
-
-        imageqt = ImageQt(self.image)
-        pixmap = QPixmap.fromImage(imageqt).copy()
-        image_field.setPixmap(pixmap)
-
-        save_button.clicked.connect(self.save_file)
-
-        widget_layout.addWidget(image_field)
-        widget_layout.addWidget(save_button)
-
-        widget.setLayout(widget_layout)
-        self.setCentralWidget(widget)
-
-    def save_file(self):
-        name = QFileDialog.getSaveFileName(
-            self,
-            "Save File",
-            filter="Images (*.ico *.jp2 *.jpg *.jpm *.jpx *.png *.tiff *.webp *.xpm)",
-        )
-        self.image.save(name[0])
+class MainWindow(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.setWindowTitle("Watermark Application")
+        layout = QGridLayout()
+        self.setLayout(layout)
+        label1 = QLabel("Widget in Tab 1.")
+        label2 = QLabel("Widget in Tab 2.")
+        tabwidget = QTabWidget()
+        tabwidget.addTab(LSBTab(), "LSB")
+        tabwidget.addTab(DCTTab(), "DCT")
+        tabwidget.addTab(AttackTab(), "ATTACKS")
+        layout.addWidget(tabwidget, 0, 0)
 
 
-class MainWindow(QMainWindow):
-    original_image_path: str = ""
+class LSBTab(QWidget):
+    pre_encode_image_path: str = ""
+    pre_decode_image_path: str = ""
     new_image: Image = None
     watermark_path: str = ""
     selected_component = 0
@@ -67,11 +51,8 @@ class MainWindow(QMainWindow):
     image_windows: List[Image.Image] = []
 
     def __init__(self):
-        super().__init__()
-        self.setWindowTitle("WatermarkApp")
+        QWidget.__init__(self)
 
-        self.widget = QWidget()
-        self.applayout = QVBoxLayout()
         self.pagelayout = QGridLayout()
         self.topframe = QFrame()
         self.encodeframe = QFrame()
@@ -87,9 +68,15 @@ class MainWindow(QMainWindow):
         self.encode_radio = QRadioButton("Encode")
         self.decode_radio = QRadioButton("Decode")
         self.encode_image_button = QPushButton("Select Image")
-        self.encode_image_label = QLabel()
+        self.pre_encode_image_field = QLabel()
+        self.pre_encode_image_label = QLabel()
         self.watermark_button = QPushButton("Select Watermark")
+        self.watermark_field = QLabel()
         self.watermark_label = QLabel()
+        self.new_image_field = QLabel()
+        self.new_image_button = QPushButton("Save")
+        self.pre_decode_image_field = QLabel()
+        self.pre_decode_image_label = QLabel()
         self.encode_button = QPushButton("Encode")
         self.decode_image_button = QPushButton("Select Image")
         self.decode_image_label = QLabel()
@@ -114,13 +101,14 @@ class MainWindow(QMainWindow):
         # Connect signals to the methods.
         self.rgb_combobox.activated.connect(self.on_combobox_activated)
         self.depth_slider.valueChanged.connect(self.on_slider_changed)
-        self.encode_image_button.clicked.connect(self.open_file)
-        self.decode_image_button.clicked.connect(self.open_file)
-        self.watermark_button.clicked.connect(self.open_file)
+        self.encode_image_button.clicked.connect(self.open_file_encoding)
+        self.decode_image_button.clicked.connect(self.open_file_decoding)
+        self.watermark_button.clicked.connect(self.open_file_encoding)
         self.encode_radio.toggled.connect(self.on_radio_selected)
         self.decode_radio.toggled.connect(self.on_radio_selected)
         self.encode_button.clicked.connect(self.encode)
         self.decode_button.clicked.connect(self.decode)
+        self.new_image_button.clicked.connect(self.save_image)
 
         self.toplayout.addWidget(self.rgb_label, 0, 0)
         self.toplayout.addWidget(self.rgb_combobox, 1, 0)
@@ -131,22 +119,35 @@ class MainWindow(QMainWindow):
 
         self.encodelayout.addWidget(self.encode_image_button, 0, 0)
         self.encodelayout.addWidget(self.watermark_button, 0, 1)
-        self.encodelayout.addWidget(self.encode_image_label, 1, 0)
-        self.encodelayout.addWidget(self.watermark_label, 1, 1)
-        self.encodelayout.addWidget(self.encode_button, 2, 0, 1, 2)
+        self.encodelayout.addWidget(self.pre_encode_image_field, 1, 0)
+        self.encodelayout.addWidget(
+            self.pre_encode_image_label, 2, 0, Qt.AlignmentFlag.AlignCenter
+        )
+        self.encodelayout.addWidget(self.watermark_field, 1, 1)
+        self.encodelayout.addWidget(
+            self.watermark_label, 2, 1, Qt.AlignmentFlag.AlignCenter
+        )
+        self.encodelayout.addWidget(self.encode_button, 3, 0, 1, 2)
 
         self.decodelayout.addWidget(self.decode_image_button, 0, 0)
         self.decodelayout.addWidget(self.decode_image_label, 0, 1)
-        self.decodelayout.addWidget(self.decode_button, 1, 0, 1, 2)
+        self.decodelayout.addWidget(
+            self.pre_decode_image_field, 1, 0, 1, 2, Qt.AlignmentFlag.AlignCenter
+        )
+        self.decodelayout.addWidget(
+            self.pre_decode_image_label, 2, 0, 1, 2, Qt.AlignmentFlag.AlignCenter
+        )
+        self.decodelayout.addWidget(self.decode_button, 3, 0, 1, 2)
 
-        self.pagelayout.addWidget(self.topframe)
-        self.pagelayout.addWidget(self.encodeframe)
-        self.pagelayout.addWidget(self.decodeframe)
+        self.pagelayout.addWidget(self.topframe, 0, 0)
+        self.pagelayout.addWidget(self.encodeframe, 1, 0)
+        self.pagelayout.addWidget(self.decodeframe, 1, 0)
+        self.pagelayout.addWidget(self.new_image_field, 0, 1, 2, 1)
+        self.pagelayout.addWidget(self.new_image_button, 2, 1, 1, 1)
+        self.new_image_field.hide()
+        self.new_image_button.hide()
 
-        self.applayout.addLayout(self.pagelayout)
-
-        self.widget.setLayout(self.applayout)
-        self.setCentralWidget(self.widget)
+        self.setLayout(self.pagelayout)
 
     def on_radio_selected(self):
         radio_button = self.sender()
@@ -154,9 +155,24 @@ class MainWindow(QMainWindow):
         if radio_button.isChecked():
             match radio_button.text():
                 case "Encode":
+                    self.pre_decode_image_field.clear()
+                    self.pre_decode_image_label.clear()
+                    self.pre_decode_image_path = ""
+                    self.new_image: Image = None
+                    self.new_image_field.clear()
+                    self.new_image_button.hide()
                     self.decodeframe.hide()
                     self.encodeframe.show()
                 case "Decode":
+                    self.pre_encode_image_field.clear()
+                    self.pre_encode_image_label.clear()
+                    self.pre_encode_image_path = ""
+                    self.watermark_field.clear()
+                    self.watermark_label.clear()
+                    self.watermark_path = ""
+                    self.new_image: Image = None
+                    self.new_image_field.clear()
+                    self.new_image_button.hide()
                     self.encodeframe.hide()
                     self.decodeframe.show()
                 case _:
@@ -169,7 +185,20 @@ class MainWindow(QMainWindow):
         self.selected_depth = index
         self.depth_label.setText(f"Watermark depth: {index}")
 
-    def open_file(self):
+    def show_resized_image(
+        self, component: QLabel, label: QLabel, pixmap: QPixmap, text: str, size: int
+    ):
+        component.clear()
+        if pixmap.size().height() > size:
+            pixmap = pixmap.scaledToHeight(size)
+        if pixmap.size().width() > size:
+            pixmap = pixmap.scaledToWidth(size)
+        component.setFixedSize(size, size)
+        component.setPixmap(pixmap)
+        component.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        label.setText(text)
+
+    def open_file_encoding(self):
         push_button = self.sender()
         result = QFileDialog.getOpenFileName(
             self,
@@ -180,18 +209,50 @@ class MainWindow(QMainWindow):
 
         match push_button.text():
             case "Select Image":
-                self.encode_image_label.setText(os.path.basename(file_path))
-                self.decode_image_label.setText(os.path.basename(file_path))
-                self.original_image_path = file_path
+                self.pre_encode_image_path = file_path
+                image_pixmap = QPixmap(self.pre_encode_image_path)
+                self.show_resized_image(
+                    self.pre_encode_image_field,
+                    self.pre_encode_image_label,
+                    image_pixmap,
+                    os.path.basename(file_path),
+                    128,
+                )
+
             case "Select Watermark":
-                self.watermark_label.setText(os.path.basename(file_path))
                 self.watermark_path = file_path
+                watermark_pixmap = QPixmap(self.watermark_path)
+                self.show_resized_image(
+                    self.watermark_field,
+                    self.watermark_label,
+                    watermark_pixmap,
+                    os.path.basename(file_path),
+                    128,
+                )
             case _:
                 raise ValueError("Invalid PushButton text")
 
+    def open_file_decoding(self):
+        result = QFileDialog.getOpenFileName(
+            self,
+            "Select File",
+            filter="Images (*.ico *.jp2 *.jpg *.jpm *.jpx *.png *.tiff *.webp *.xpm)",
+        )
+        file_path = result[0]
+
+        self.pre_decode_image_path = file_path
+        image_pixmap = QPixmap(self.pre_decode_image_path)
+        self.show_resized_image(
+            self.pre_decode_image_field,
+            self.pre_decode_image_label,
+            image_pixmap,
+            os.path.basename(file_path),
+            256,
+        )
+
     def encode(self):
-        if self.original_image_path is not None and self.watermark_path is not None:
-            self.original_image = Image.open(self.original_image_path)
+        if self.pre_encode_image_path is not None and self.watermark_path is not None:
+            self.original_image = Image.open(self.pre_encode_image_path)
             self.watermark = Image.open(self.watermark_path)
 
             image_data = ImageData(self.original_image)
@@ -200,29 +261,62 @@ class MainWindow(QMainWindow):
                 self.watermark,
                 self.selected_depth,
             )
+            self.new_image = image_data.rgb2image()
 
-            w1 = ImageWindow(image_data.original_image, title="Original")
-            self.image_windows.append(w1)
-            w1.show()
-            w2 = ImageWindow(image_data.rgb2image(), title="Watermarked")
-            self.image_windows.append(w2)
-            w2.show()
+            imageqt = ImageQt(self.new_image)
+            pixmap = QPixmap.fromImage(imageqt).copy()
+            self.new_image_field.setFixedSize(pixmap.size())
+            self.new_image_field.setPixmap(pixmap)
+            self.new_image_field.show()
+            self.new_image_button.show()
 
     def decode(self):
-        if self.original_image_path is not None:
-            self.original_image = Image.open(self.original_image_path)
+        if self.pre_decode_image_path is not None:
+            self.original_image = Image.open(self.pre_decode_image_path)
             image_data = ImageData(self.original_image)
-            extracted = image_data.lsb_decode(
+            self.new_image = image_data.lsb_decode(
                 ImageComponent(self.selected_component),
                 self.selected_depth,
             )
 
-            w1 = ImageWindow(self.original_image, title="Original")
-            w2 = ImageWindow(extracted, title="Extracted")
-            self.image_windows.append(w1)
-            self.image_windows.append(w2)
-            w1.show()
-            w2.show()
+            imageqt = ImageQt(self.new_image)
+            pixmap = QPixmap.fromImage(imageqt).copy()
+            self.new_image_field.setFixedSize(pixmap.size())
+            self.new_image_field.setPixmap(pixmap)
+            self.new_image_field.show()
+            self.new_image_button.show()
+
+    def save_image(self):
+        name = QFileDialog.getSaveFileName(
+            self,
+            "Save File",
+            filter="Images (*.ico *.jp2 *.jpg *.jpm *.jpx *.png *.tiff *.webp *.xpm)",
+        )
+        self.new_image.save(name[0])
+
+
+class DCTTab(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.pagelayout = QGridLayout()
+
+        self.set_properties()
+
+    def set_properties(self):
+        self.setLayout(self.pagelayout)
+
+
+class AttackTab(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.pagelayout = QGridLayout()
+
+        self.set_properties()
+
+    def set_properties(self):
+        self.setLayout(self.pagelayout)
 
 
 app = QApplication(sys.argv)
